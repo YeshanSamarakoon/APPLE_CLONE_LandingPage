550c0df6616d676320cb762c6927e585696cefed
YESHABI <yeshanbi.samarakoon@gmail.com>
2025-01-15 17:10:42 +0530

    new commit7

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Highlights from "./component/Highlights"
 import Model from "./component/Model"
 import Navbar from "./component/Navbar"
 import Features from "./component/Features"
+import HowitWorks from "./component/HowitWorks"
+import Footer from "./component/Footer"
 
 const App = () => {
  
@@ -14,6 +16,8 @@ const App = () => {
       <Highlights/>
       <Model/>
       <Features/>
+      <HowitWorks/>
+      <Footer/>
     </main>
   )
 }
