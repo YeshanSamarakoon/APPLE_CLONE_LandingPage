f6124f6ca861d38dc2f87956c8edc3a14606dbb7
YESHABI <yeshanbi.samarakoon@gmail.com>
2025-04-04 21:04:10 +0530

    change layout  commit 16

--- src/component/videoCarosel.jsx
+++ src/component/videoCarosel.jsx
@@ -149,7 +149,7 @@ const VideoCarousel = () => {
         {hightlightsSlides.map((list, i) => (
           <div key={list.id} id="slider" className="sm:pr-20 pr-10">
             <div className="video-carousel_container">
-              <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-white">
+              <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
                 <video
                   id="video"
                   playsInline={true}
